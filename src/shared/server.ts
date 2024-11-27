92ed4f71324ae13ca08586bd7b2e7b3033fa0d24
HenriqueGMen <henrimeen24@gmail.com>
2024-11-27 00:32:48 -0300

    adicionando tratamento de erros

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -1,11 +1,26 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import { routes } from './http'
+import AppError from './errors/appError'
 
 const app = express()
 
 app.use(cors())
 app.use(routes)
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
 
 app.listen(3333, () => {
   console.log('Server is running!!!')
