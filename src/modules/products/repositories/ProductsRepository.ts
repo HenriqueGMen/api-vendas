168f86514318802b115039ef5d3c01a14212f5f8
HenriqueGMen <henrimeen24@gmail.com>
2024-12-21 00:55:27 -0300

    feat: criação de repositório e dto produtos

---
+++ src/modules/products/repositories/ProductsRepository.ts
@@ -0,0 +1,25 @@
+import { prisma } from "@shared/database/prismaClient";
+import { Product } from "../entities/Product";
+import { ICreateProductDTO } from "../dtos/ICreateProduct.dto";
+
+export class ProductsRepository {
+  public async create({ name, price, quantity }: ICreateProductDTO): Promise<Product> {
+    const newProduct = await prisma.products.create({
+      data: {
+        name,
+        price,
+        quantity
+      }
+    })
+
+    return newProduct
+  }
+  
+  public async findByName(name: string): Promise<Product[]> {
+    const product = await prisma.products.findMany({
+      where: { name }
+    })
+
+    return product
+  }
+}
